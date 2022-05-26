95b4e93cbe13ee3277197ab03b16806f93f7117d
이동주 <joey0319@naver.com>
2022-05-26 20:14:23 +0900

    home 수정

--- final-pjt-front/src/views/HomeView.vue
+++ final-pjt-front/src/views/HomeView.vue
@@ -1,31 +1,31 @@
 <template>
   <div class="container">
-    <div class="row justify-content-center">
+    <div>
       <b-img id="mainImg" :src="require(`@/assets/movie_home_main.jpg`)" fluid alt="Responsive image"></b-img>
     </div>
     <br>
     <b-card-group deck>
-      <router-link :to="{ name: 'articles', params: {} }">
-        <b-card title="Community" img-src="@/assets/community.png" img-alt="Image" img-top>
+      <a href="http://localhost:8080/articles">
+        <b-card title="Community" img-src=http://www.stpeters.es/blog/wp-content/uploads/2022/05/community.png img-alt="Image" img-top img-height="200" >
           <b-card-text>
-            사용자들과 영화에 대한 이야기를 나눠보세요.
+            영화에 대한 이야기를 나눠보세요.
           </b-card-text>
-        </b-card> 
-      </router-link>
-      <router-link :to="{ name: 'movieList', params: {} }">
-        <b-card title="Rating" img-src="@/src/assets/rating.png" img-alt="Image" img-top>
+        </b-card>
+      </a>
+      <a href="http://localhost:8080/movies">
+        <b-card title="Rating" img-src=https://assets-global.website-files.com/5eff9c5e4dba181f8aa2d1e0/614dfaed9cdcb08628f3b1cc_5-star-rating.jpeg img-alt="Image" img-height="200">
           <b-card-text>
             좋아하는 영화에 평점을 남겨보세요.
           </b-card-text>
         </b-card>
-      </router-link>
-      <router-link :to="{ name: 'movieRecommendList', params: {} }">
-        <b-card title="Recommend" img-src="@/scr/assets/recommend.png" img-alt="Image" img-top>
+      </a>
+      <a href="http://localhost:8080/recommendation">
+        <b-card title="Recommend" img-src=https://pbs.twimg.com/media/DfvrcdUWAAIIuGi.jpg img-alt="Image" img-top img-height="200">
           <b-card-text>
-            좋아하는 장르의 영화 추천을 받아보세요
+            좋아하는 장르의 영화를 추천 받아보세요
           </b-card-text>
         </b-card>
-      </router-link>
+      </a>
     </b-card-group>
   </div>
 </template>
@@ -38,9 +38,6 @@ export default {
 
 <style>
   #mainImg {
-    width: 900px;
-  }
-  .router-link {
-    width: 300px;
+    width: 1050px;
   }
 </style>
