28b0e194b4b9685e9f9c5955d930bde72dd35711
박세호 <seho27060@gmail.com>
2022-05-26 12:32:07 +0900

    0526 first commit

--- final-pjt-front/src/views/ArticleDetailView.vue
+++ final-pjt-front/src/views/ArticleDetailView.vue
@@ -1,19 +1,35 @@
 <template>
   <div>
     <h1>{{ article.title }}</h1>
-
+    <hr>
     <p>
       {{ article.content }}
     </p>
     <!-- Article Edit/Delete UI -->
-    <div v-if="isAuthor">
-      <router-link :to="{ name: 'articleEdit', params: { articlePk } }">
-        <button>Edit</button>
-      </router-link>
-      |
-      <button @click="deleteArticle(articlePk)">Delete</button>
-    </div>
 
+    <div class="container">
+      <b-card :title="title" >
+      <span class="d-inline">      
+        <p class="h6"><b-icon icon="person-fill" style="float:left;"></b-icon></p>
+        <a :href="userProfileUrl">
+          <b-card-sub-title :sub-title="username"></b-card-sub-title>
+        </a>
+      </span>
+      <hr>
+        <b-card-text>
+          {{ article.content }}
+        </b-card-text>
+      <div v-if="isAuthor">
+        <router-link :to="{ name: 'articleEdit', params: { articlePk } }">
+          <button>Edit</button>
+        </router-link>
+        |
+        <button @click="deleteArticle(articlePk)">Delete</button>
+      </div>
+        <!-- <a href="#" class="card-link">Card link</a>
+        <b-link href="#" class="card-link">Another link</b-link> -->
+      </b-card>
+    </div>
     <!-- Article Like UI -->
     <div>
       Likeit:
@@ -47,7 +63,17 @@
       ...mapGetters(['isAuthor', 'article']),
       likeCount() {
         return this.article.like_users?.length
-      }
+      },
+      title(){
+        return this.article.title
+      },
+      username() {
+        return this.article.user.username
+      },
+      userProfileUrl(){
+        return `http://localhost:8080/profile/" + ${this.username}`
+      },
+
     },
     methods: {
       ...mapActions([
--- final-pjt-front/src/views/ArticleListView.vue
+++ final-pjt-front/src/views/ArticleListView.vue
@@ -3,23 +3,35 @@
     <h1>Home</h1>
     <router-link :to="{ name: 'articleNew' }">새글작성</router-link>
     <ul>
-      <li v-for="article in articles" :key="article.pk">
-        <!-- 작성자 -->
-        {{ article.user.username }} : 
 
-        <!-- 글 이동 링크 (제목) -->
+      <li v-for="article in articles" :key="article.pk">
+        {{ article.user.username }} : 
         <router-link 
           :to="{ name: 'article', params: {articlePk: article.pk} }">
           {{ article.title }}
         </router-link>
-
-        <!-- 댓글 개수/좋아요 개수 -->
         =>
         ({{ article.comment_count }}) | +{{ article.like_count }}
 
       </li>
-    </ul>
-   
+    </ul>      
+    <b-list-group-item class="d-flex justify-content-between align-items-center">
+        작성자 :         
+        <div>제목</div>
+        <b-badge variant="primary" pill>댓글</b-badge>
+        <b-badge variant="danger" pill>좋아요</b-badge>
+      </b-list-group-item>
+    <b-list-group v-for="article in articles" :key="article.pk">
+      <b-list-group-item class="d-flex justify-content-between align-items-center">
+        {{ article.user.username }} :         
+        <router-link 
+          :to="{ name: 'article', params: {articlePk: article.pk} }">
+          {{ article.title }}
+        </router-link>
+        <b-badge variant="primary" pill>{{ article.comment_count }}</b-badge>
+        <b-badge variant="danger" pill>{{ article.like_count }}</b-badge>
+      </b-list-group-item>
+    </b-list-group>
   </div>
 </template>
 
@@ -30,6 +42,10 @@
     name: 'ArticleList',
     computed: {
       ...mapGetters(['articles'])
+      ,
+      profileUrl (user) {
+        return `http://localhost:8080/profile/" + ${user}`
+      },
     },
     methods: {
       ...mapActions(['fetchArticles'])
