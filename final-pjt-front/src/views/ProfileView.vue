1eb5a6e50fc75ddf33d925526b3894e3aa04716f
박세호 <seho27060@gmail.com>
2022-05-26 21:00:44 +0900

    0525 fe ~ing6

--- final-pjt-front/src/views/ProfileView.vue
+++ final-pjt-front/src/views/ProfileView.vue
@@ -1,23 +1,36 @@
 <template>
   <div class="container justify-content-center">
     <b-list-group>
-      <b-list-group-item class="text-center"><h1>{{ profile.username }}의 프로필</h1></b-list-group-item>
       <b-list-group-item>
-        <h3>작성한 글</h3>
-        {{ profile.articles.length}}
-        <b-icon icon="box-arrow-in-right" animation="cylon" font-scale="1" style="color:blue"></b-icon>
-        <b-table striped hover responsive sticky-header="300px"
-        :items="profile.articles" :fields="fields" 
-        class="text-center" id="my-article">
-        </b-table>
-        <b-pagination
-          v-model="currentPage"
-          :total-rows="articleRows"
-          :per-page="perPage"
-          aria-controls="my-article"
-          class="col justify-content-center">
-        </b-pagination>
+        <h1>{{ profile.username }}의 프로필</h1>
       </b-list-group-item>
+      <!-- <b-list-group-item> -->
+      <b-container fluid>
+          <h3>작성한 글</h3>
+          <p class="mt-3">Current Page: {{ currentPage }}</p>
+          {{ profile.articles.length}}
+          <b-icon icon="box-arrow-in-right" animation="cylon" font-scale="1" style="color:blue"></b-icon>
+          <b-table striped hover responsive sticky-header="300px"
+          :items="profile.articles" :fields="fields" 
+          class="text-center" id="my-article">
+            <template #cell(title)="data">
+              <router-link 
+                :to="{ name: 'article', params: {articlePk: data.item.pk} }"
+                style="color:black">
+                {{ data.item.title }}
+              </router-link>
+            </template>
+
+          </b-table>
+          <b-pagination
+            v-model="articlePerPage"
+            :total-rows="articleRows"
+            :per-page="articleCurrentPage"
+            aria-controls="my-article"
+            class="col justify-content-center">
+          </b-pagination>
+        </b-container>
+      <!-- </b-list-group-item> -->
       <br>
       
       <b-list-group-item><h3>좋아요한 글</h3>
@@ -51,8 +64,8 @@ export default {
   name: 'ProfileView',
   data(){
       return{
-        perPage: 5,
-        currentPage: 1,
+        articlePerPage: 5,
+        articleCurrentPage: 1,
         fields:[
           { key: "title", label: "제목"},
         ]
